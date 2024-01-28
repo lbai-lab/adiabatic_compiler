ee94f9b9bdf78007ba64e565b656e14d0a4c1d1e
sunnytan53 <27101659+sunnytan53@users.noreply.github.com>
2024-01-27 17:18:17 -0800

    docs setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Adiabatic Compiler'
-copyright = '2024, Haoyuan Tan & Daniel Huang'
-author = 'Haoyuan Tan & Daniel Huang'
-release = '0.1'
+project = "Adiabatic Compiler"
+copyright = "2024, Haoyuan Tan & Daniel Huang"
+author = "Haoyuan Tan & Daniel Huang"
+release = "0.1"
 
 # -- General configuration
 extensions = [
@@ -21,13 +21,23 @@ extensions = [
     "nbsphinx",
     "sphinx.ext.napoleon",
 ]
+
+# -- Options for HTML output
+html_theme = "sphinx_rtd_theme"
+
+
+# autodoc_mock_imports = ["tqdm", "numpy"]
+# intersphinx_mapping = {
+#     "python": ("https://docs.python.org/3/", None),
+#     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+# }
+# intersphinx_disabled_domains = ["std"]
+# nbsphinx_allow_errors = True
+# templates_path = ["_templates"]
+
+# # -- Options for EPUB output
+# epub_show_urls = "footnote"
+# autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
 # templates_path = ['_templates']
 # exclude_patterns = []
-
-
-
-# # -- Options for HTML output -------------------------------------------------
-# # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-# html_theme = 'alabaster'
-# html_static_path = ['_static']
