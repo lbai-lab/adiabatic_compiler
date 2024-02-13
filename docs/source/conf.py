ff664c458ac058ddda069d7d449112a8b070a0fe
sunnytan53 <27101659+sunnytan53@users.noreply.github.com>
2024-02-13 08:37:12 -0800

    quickfix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath("../../ac"))
+sys.path.append(os.path.abspath("../.."))
 
 project = "Adiabatic Compiler"
 copyright = "2024, Haoyuan Tan & Daniel Huang"
