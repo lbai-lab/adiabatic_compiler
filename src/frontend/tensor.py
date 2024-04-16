74988966278e4f1b2d1f1d3ea0757d80397c26ff
sunnytan53 <haoyuan.t@outlook.com>
2024-04-15 18:33:12 -0700

    another failed attempt on tensor network

--- src/frontend/tensor.py
+++ src/frontend/tensor.py
@@ -1,5 +1,9 @@
-import numpy as np
+import math
+
 import scipy.sparse as sp
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import UnitaryGate
+from qiskit.quantum_info import Operator
 
 from frontend import *
 
@@ -20,30 +24,26 @@ class TensorAdiabaticProgram(AdiabaticProgram):
 
 
 class TensorFrontend(Frontend):
-
-    def __init__(self, eps: float = 0.1):
-        phi_I = ((np.array([[1], [0], [0], [1]])) / np.sqrt(2)).astype(np.complex128)
-        phi_X = np.dot(np.kron(np.eye(2), np.array([[0, 1], [1, 0]])), phi_I)
-        phi_Z = np.dot(np.kron(np.eye(2), np.array([[1, 0], [0, -1]])), phi_I)
-        phi_XZ = np.dot(
-            np.kron(
-                np.eye(2),
-                np.dot(np.array([[0, 1], [1, 0]]), np.array([[1, 0], [0, -1]])),
-            ),
-            phi_I,
+    __phi_I = (sp.csc_matrix([[1], [0], [0], [1]])) / math.sqrt(2)
+    __phi_X = sp.kron(sp.eye(2), sp.csc_matrix([[0, 1], [1, 0]])) @ __phi_I
+    __phi_Z = sp.kron(sp.eye(2), sp.csc_matrix([[1, 0], [0, -1]])) @ __phi_I
+    __phi_XZ = (
+        sp.kron(
+            sp.eye(2),
+            sp.csc_matrix([[0, 1], [1, 0]]) @ sp.csc_matrix([[1, 0], [0, -1]]),
         )
+        @ __phi_I
+    )
 
+    def __init__(self, eps: float = 2):
         self.lamb = sp.csc_matrix(
-            eps * np.outer(phi_I, phi_I)
+            eps * self.project(self.__phi_I)
             + (
-                np.outer(phi_X, phi_X)
-                + np.outer(phi_Z, phi_Z)
-                + np.outer(phi_XZ, phi_XZ)
+                self.project(self.__phi_X)
+                + self.project(self.__phi_Z)
+                + self.project(self.__phi_XZ)
             )
         )
-
-        bell_state = set(int(x, 2) for x in ["0000", "0101", "1010", "1111"])
-        self.bell = sp.csc_matrix([[0.5 if i in bell_state else 0] for i in range(16)])
 
     def lamb_n(self, n: int) -> sp.csc_matrix:
         res = self.lamb
@@ -51,57 +51,86 @@ class TensorFrontend(Frontend):
             res = sp.kron(res, self.lamb)
         return sp.csc_matrix(res)
 
-    def project_phi_U(self, U: sp.csc_matrix) -> sp.csc_matrix:
-        phi_U = sp.kron(sp.eye(4), U).dot(self.bell)
-        return sp.csc_matrix(phi_U.dot(phi_U.T))
+    def project(self, arr) -> sp.csc_matrix:
+        arr = sp.csc_matrix(arr).reshape((-1, 1))
+        return arr @ arr.T
 
     def gen_H_in(self, n: int, D: int) -> sp.csc_matrix:
+        in_columns = 2 * n
         lamb = self.lamb_n(n)
-        size_one_column = 2**n
-        penalize_one = [bin(i)[2:].count("1") for i in range(size_one_column)]
+
+        states = []
+        for i in range(2**in_columns):
+            count = 0
+            b = bin(i)[2:].zfill(in_columns)
+            for j in range(0, in_columns, 2):
+                if b[j] == "1":
+                    count += 1
+
+            states.append(count)
 
         return sp.csc_matrix(
-            sp.kron(
-                lamb.dot(sp.kron(sp.diags(penalize_one), sp.eye(size_one_column))).dot(
-                    lamb
-                ),
-                sp.eye(2 ** ((2 * D - 1) * n)),
-            )
+            sp.kron(lamb @ self.project(states) @ lamb, sp.eye(2 ** (n * (2 * D - 1))))
         )
 
     def gen_H_out(self, n: int, D: int) -> sp.csc_matrix:
-        penalize_one = [bin(i)[2:].count("1") for i in range(2**n)]
+        states = [bin(i)[2:].count("1") for i in range(2**n)]
 
-        return sp.csc_matrix(
-            sp.kron(
-                sp.eye(2 ** (2 * D * n)),
-                sp.diags(penalize_one),
-            )
+        return sp.csc_matrix(sp.kron(sp.eye(2 ** (n * 2 * D)), self.project(states)))
+
+    def encode_U(self, U: sp.spmatrix, is_boundary: bool) -> sp.csc_matrix:
+        qc = QuantumCircuit(4)
+        qc.append(UnitaryGate(U.toarray()), [1, 3])
+        encoded_U = sp.eye(16) - self.project(
+            Operator(qc).data @ sp.kron(self.__phi_I, self.__phi_I)
         )
 
-    def gen_H_prop(self, n: int, D: int, Us: list[sp.csc_matrix]) -> sp.csc_matrix:
-        total = n * (2 * D + 1)
+        if is_boundary:
+            qc = QuantumCircuit(6)
+            qc.append(UnitaryGate(encoded_U.toarray(), check_input=False), [1, 3, 4, 5])
+        else:
+            qc = QuantumCircuit(8)
+            qc.append(UnitaryGate(encoded_U.toarray(), check_input=False), [1, 3, 4, 6])
 
+        return sp.csc_matrix(Operator(qc))
+
+    def gen_H_prop(self, n: int, D: int, Us: list[sp.spmatrix]) -> sp.csc_matrix:
+        columns = 2 * D + 1
+        total = n * columns
+
+        # last unitary
         lamb_end = sp.kron(self.lamb_n(2), sp.eye(4))
-        h_u = lamb_end.dot(
-            sp.eye(64) - sp.kron(sp.eye(4), self.project_phi_U(Us[-1]))
-        ).dot(lamb_end)
-        h_u_sum = sp.kron(sp.eye(2 ** (total - 6)), h_u)
+        qc = QuantumCircuit(total)
+        qc.append(
+            UnitaryGate(
+                (lamb_end @ self.encode_U(Us[-1], True) @ lamb_end).toarray(),
+                check_input=False,
+            ),
+            range(total - 6, total),
+        )
 
+        h_u_sum = sp.csc_matrix(Operator(qc))
+
+        # non-last unitary
         lamb_4 = self.lamb_n(4)
-        for i, U in enumerate(Us[:-1]):
-            h_u = lamb_4.dot(
-                sp.eye(256)
-                - sp.kron(sp.eye(4), sp.kron(self.project_phi_U(U), sp.eye(4)))
-            ).dot(lamb_4)
-            h_u_sum += sp.kron(
-                sp.eye(2 ** (4 * i)), sp.kron(h_u, sp.eye(2 ** (total - 4 * i - 8)))
+        for i in range(D - 1):
+            q_i = 2 * i
+
+            qc = QuantumCircuit(total)
+            qc.append(
+                UnitaryGate(
+                    (lamb_4 @ self.encode_U(Us[i], False) @ lamb_4).toarray(),
+                    check_input=False,
+                ),
+                range(q_i, q_i + 8),
             )
 
-        return sp.csc_matrix(h_u_sum)
+            h_u_sum += sp.csc_matrix(Operator(qc))
+
+        return h_u_sum
 
     def unitaries_to_program(
-        self, Us: list[sp.csc_matrix], num_qubits: int, depth: int
+        self, Us: list[sp.spmatrix], num_qubits: int, depth: int
     ) -> TensorAdiabaticProgram:
         if len(Us) < 1:
             raise ValueError(f"Require at least one unitary matrix")
@@ -112,6 +141,4 @@ class TensorFrontend(Frontend):
         H_prop = self.gen_H_prop(num_qubits, depth, Us)
 
         total = num_qubits * (2 * depth + 1)
-        return TensorAdiabaticProgram(
-            H_in, H_in + H_out + H_prop, 2**total, total, total
-        )
+        return TensorAdiabaticProgram(H_in, H_prop + H_out, 2**total, total, total)
--- src/interpreter/tensor.py
+++ src/interpreter/tensor.py
@@ -10,25 +10,36 @@ from backend.cpu import CPUBackend
 
 class TensorInterpreter(Interpreter):
 
-    def run(self, qc: QuantumCircuit, num_shots=1024) -> dict:
+    def run(self, qc: QuantumCircuit, num_shots=10000) -> dict:
         gates = compress_circuit(qc, Compress.parallel)[0]
         unitaries = [sp.csc_matrix(Operator(x)) for x in gates]
 
         # TODO: for n-qubit case, calculate the actual depth
         n = qc.num_qubits
-        program = TensorFrontend().unitaries_to_program(unitaries, n, len(unitaries))
+        D = len(unitaries)
+        program = TensorFrontend().unitaries_to_program(unitaries, n, D)
 
         results = {}
-        for res, count in CPUBackend().run(program, 1024).items():
-            real_res = res[-n:]
-            results.setdefault(real_res, 0)
-            results[real_res] += count
+        valid_states = ["00", "11"]
+        for res, count in CPUBackend().run(program, num_shots).items():
+            for i in range(2, 2 * n * D + 1, 2):
+                if res[i - 2 : i] not in valid_states:
+                    break
+            else:
+                results.setdefault(res[-n:], 0)
+                results[res[-n:]] += count
 
-        print(results)
+        return results
 
 
 qc = QuantumCircuit(2)
-qc.x(0)
-qc.x(1)
+qc.x([0])
+print(sorted(TensorInterpreter().run(qc).items()))
 
-TensorInterpreter().run(qc)
+qc = QuantumCircuit(2)
+qc.x([1])
+print(sorted(TensorInterpreter().run(qc).items()))
+
+qc = QuantumCircuit(2)
+qc.x([0, 1])
+print(sorted(TensorInterpreter().run(qc).items()))
